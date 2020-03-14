b4ead5efeb7bee98c45245af51c660b9f4f1fd66
KevinZ2Z <1185229277@qq.com>
2020-03-14 21:17:03 +0800

    Create ChinaDAO.java

---
+++ Web/src/dao/ChinaDAO.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pojo.China;
+import servlet.DBUtil;
+
+public class ChinaDAO
+{
+	public List<China> listSomeDay(int nums) {
+		List<China> chinaDatas = null;
+		String sql = " SELECT * FROM nation ORDER BY date DESC LIMIT " + nums;
+			
+        try (Connection c = DBUtil.getConnection();PreparedStatement ps = c.prepareStatement(sql)) {
+        	China chinaData = null;
+        	chinaDatas = new ArrayList<China>();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+            	chinaData = new China();
+            	
+            	chinaData.setDate(rs.getDate("date"));
+            	chinaData.setConfirmedNum(rs.getInt("confirmedNum"));
+            	chinaData.setSuspectedNum(rs.getInt("suspectedNum"));
+            	chinaData.setCuresNum(rs.getInt("curesNum"));
+            	chinaData.setDeathsNum(rs.getInt("deathsNum"));
+            	chinaData.setTreating(rs.getInt("treatingNum"));
+            	
+            	chinaDatas.add(chinaData);
+            }
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+		
+		
+       return chinaDatas;
+
+    }
+}
